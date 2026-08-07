4da3bcd5be5c1855f6bea60056af51174b3092fe
ABHILASH BORRA <Abhilash113131@gmail.com>
2026-08-07 21:43:43 +0530

    AnonymousFunction

---
+++ Day7/AnonymousFunction.ts
@@ -0,0 +1,45 @@
+/* 
+Anonymous Function --- Unnamed function / nameless function
+
+Anonymous Function is a function that does not have a name.
+Instead it is assigned to a variable which acts as its name
+
+syntax:
+
+let variable_name = function(parameters): returnType
+{
+  // code to execute
+}
+variable_name(arguments); // calling the function
+
+*/
+
+// Example 1: Anonymous function with no parameters and no return value.
+/* 
+let greet = function()
+{
+    console.log("hello, welcome to Anonymous function with no parameters and no return value.")
+}
+greet();
+
+ */
+
+// Example 2: Anonymous function with parameters and no return value.
+/* 
+let printmessage = function(mesge:string)
+{
+    console.log(mesge)
+}
+printmessage("Hello, welcome to Anonymous function with parameters and no return value.")
+
+ */
+
+// Example 3: Anonymous function with parameters and return value
+
+let addnumbers = function(x:number,y:number):number
+{
+    return x+y
+}
+let result= addnumbers(10,20)
+console.log("Result=", result)
+
